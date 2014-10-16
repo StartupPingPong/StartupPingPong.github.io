4c42e4cf44c170600da19249d3329d18662f1770
Fredric Castelius <fredric@castelius.se>
2014-10-16 21:54:43 +0200

    Tracking added

--- _/script/script.js
+++ _/script/script.js
@@ -1,13 +1,13 @@
-// Fit the embedded videos
+// fit embedded videos
 $("body").fitVids(); 
 
-// Fitted headers
+// responsive headers
 $("#hero #title").fitText(1);
 $("#sub-title").fitText(1.5);
 // $(".fitHeading").fitText();
 // $(".fitHeading3").fitText(1.1, { minFontSize: '50px', maxFontSize: '75px' });
 
-//
+// body height
 $(function() {
     $(window).resize(function() {
         $('body:last').height($(window).height() - $('body:last').offset().top);
@@ -26,7 +26,7 @@ var feed = new Instafeed({
 });
 feed.run();
 
-// fb embed
+// facebook embed
 (function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
@@ -36,7 +36,7 @@ feed.run();
 }(document, 'script', 'facebook-jssdk'));
 
 
-// Smooth scroll
+// smooth scroll
 smoothScroll.init({
   speed: 1000,
   easing: 'easeInOutCubic',
@@ -45,3 +45,12 @@ smoothScroll.init({
   callbackBefore: function ( toggle, anchor ) {},
   callbackAfter: function ( toggle, anchor ) {}
 });
+
+// tracking
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-48763619-1', 'auto');
+ga('send', 'pageview');

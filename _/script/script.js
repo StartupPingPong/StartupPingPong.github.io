385f2d51604e3304b488818388a740831ca48ddd
Fredric Castelius <fredric@castelius.se>
2014-10-31 16:41:45 +0100

    Optimzied

--- _/script/script.js
+++ _/script/script.js
@@ -1,41 +1,6 @@
 // fit embedded videos
 $("body").fitVids(); 
 
-// responsive headers
-$("#hero #title").fitText(1);
-$("#sub-title").fitText(1.5);
-// $(".fitHeading").fitText();
-// $(".fitHeading3").fitText(1.1, { minFontSize: '50px', maxFontSize: '75px' });
-
-// body height
-$(function() {
-    $(window).resize(function() {
-        $('body:last').height($(window).height() - $('body:last').offset().top);
-    });
-    $(window).resize();
-});
-
-// instag feed
-/*var feed = new Instafeed({
-    get: 'tagged',
-    tagName: 'startuppingpong',
-    clientId: 'b9eea1a951e24c54bbfd46e8360119f1',
-    resolution: 'standard_resolution',
-    sortBy: 'most-recent',
-    limit: '40'
-});
-feed.run();*/
-
-// facebook embed
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&appId=504797676310031&version=v2.0";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-
 // smooth scroll
 smoothScroll.init({
   speed: 1000,
@@ -44,11 +9,6 @@ smoothScroll.init({
   updateURL: true,
   callbackBefore: function ( toggle, anchor ) {},
   callbackAfter: function ( toggle, anchor ) {}
-});
-
-// background slider 
-$(function() {
-  cbpBGSlideshow.init();
 });
 
 // tracking

bbaef7546f2f832cdbe550774fb8cf9f70a6fcbe
Fredric Castelius <fredric@castelius.se>
2014-10-31 14:47:48 +0100

    Mobile fix

--- _/script/script.js
+++ _/script/script.js
@@ -46,6 +46,11 @@ smoothScroll.init({
   callbackAfter: function ( toggle, anchor ) {}
 });
 
+// background slider 
+$(function() {
+  cbpBGSlideshow.init();
+});
+
 // tracking
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
